21d8c7f6b8e80b27bff1486167bd0dfe4d43c895
Seamus D'Arcy <seamus@celsussolutions.com>
2012-04-13 17:25:21 +0100

    orchestra db details

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'db_8ff7d486');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'user_8ff7d486');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'oMs&IDoijnlT6@');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'a.db.shared.orchestra.io');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
